15cb82ed56332e0a3f06da311ebb35f1f893ec4d
khoa <quockhoa.bh2000@gmail.com>
2022-08-25 16:08:15 +0700

    25/08
    Hoàn thành khách hàng

--- src/app/client/new-customers/new-customers.component.ts
+++ src/app/client/new-customers/new-customers.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-new-customers',
   templateUrl: './new-customers.component.html',
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewCustomersComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private httpClient: HttpClient) { }
+  countryData
+  districtsData
+  provincescode = "thanh_pho_ha_noi"
   ngOnInit(): void {
+    this.gData();
   }
-
+  async gData() {
+    this.httpClient.get('https://provinces.open-api.vn/api/?depth=2').subscribe(i => {
+      this.countryData = i;
+      this.setProvincesCode(this.provincescode)
+    })
+  }
+  setProvincesCode(e) {
+    this.countryData?.forEach(element => {
+      if (element.codename === e) {
+        this.districtsData = element.districts
+      }
+    });
+  }
 }
