71b06c5332f4f6a9b35cc65e91bd96e4ced03a21
Nguyễn Hoài Thương <nhthuong1804@gmail.com>
2022-08-25 16:44:34 +0700

    Thay doi muc thao luan
    Thay đổi mục thảo luận

--- src/app/tasks/task-detail/task-detail-tabs/task-overview/task-overview.component.ts
+++ src/app/tasks/task-detail/task-detail-tabs/task-overview/task-overview.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartData, ChartConfiguration, ChartType, ChartOptions } from 'chart.js';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
 @Component({
   selector: 'app-task-overview',
   templateUrl: './task-overview.component.html',
   styleUrls: ['./task-overview.component.css', '../../task-detail.component.css']
 })
 export class TaskOverviewComponent implements OnInit {
+  editorConfig: AngularEditorConfig = {
+    height: '200px',
+    editable: true,
+  }
   public chartLabels: string[] = ['Còn hạn', 'Gần hạn', 'Quá hạn', 'Đề xuất kết thúc'];
   public doughnutChartData: ChartData<'doughnut'> = {
     labels: this.chartLabels,
