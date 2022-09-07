5da18c71ac0c1dd8152f51aac228bebc7cd63fe5
ngohonglam40 <ngohonglam40@gmail.com>
2022-09-07 14:23:36 +0700

    search up

--- src/app/search/search/search.component.ts
+++ src/app/search/search/search.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { title } from 'process';
 
 @Component({
   selector: 'app-search',
@@ -8,14 +10,32 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
- 
-keylabel
-  constructor(private router:Router,private activaterouter:ActivatedRoute) { }
+
+  keylabel
+  searchdata
+  result=[]
+  constructor(private router: Router, private activaterouter: ActivatedRoute, private httpClient: HttpClient) { }
 
   ngOnInit(): void {
-   this.activaterouter.params.subscribe(e=>{
-    this.keylabel=e['label']
-   })
+    this.activaterouter.params.subscribe(e => {
+      this.keylabel = e['label']
+    })
+    this.getData()
   }
 
+  getData() {
+    this.httpClient.get('https://62fde3c541165d66bfb3a622.mockapi.io/api/projectlist').subscribe(x => {
+      this.searchdata = x
+      this.search(this.keylabel)
+      console.log(this.searchdata)
+      console.log(this.result)
+    })
+  }
+  search(label) {
+    this.searchdata.forEach(element => {
+      if(element.name.search(label)>-1){
+        this.result.push(element)
+      }
+    });
+  }
 }
